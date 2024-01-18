9b706936c28dcba3fdc383a4e1c426134ee1da57
Jethro Baldonado <jethro.baldonado@gmail.com>
2024-01-19 02:22:03 +0800

    remove flex-col

--- src/components/ContainerThirds.tsx
+++ src/components/ContainerThirds.tsx
@@ -6,7 +6,7 @@ export default function ContainerThirds({ title, children }: {
 }): React.ReactNode {
     return (
         <div className="container flex pt-20 justify-center justify-items-center">
-            <div className="flex sm:flex-col w-2/3">
+            <div className="flex w-2/3">
                 <div className="w-1/4 sm:w-full">
                     <h4 className="text-2xl font-serif text-altaDarkOrange">{title}</h4>
                 </div>
