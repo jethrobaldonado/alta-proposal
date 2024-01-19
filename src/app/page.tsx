133f52f2360b065d8d6558d7c04a98e9a221c4a0
Jethro Baldonado <jethro.baldonado@gmail.com>
2024-01-19 12:02:51 +0800

    remove animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React, { useRef } from 'react'
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import React, {useRef} from 'react'
+import {Parallax, ParallaxLayer, IParallax} from '@react-spring/parallax'
 import Banner from "@/components/Banner";
 import Welcome from "@/components/Welcome";
 import Story from "@/components/Story";
@@ -15,54 +15,16 @@ export default function Home() {
     const parallax = useRef<IParallax>(null!);
 
     return (
-        <main className="w-screen h-screen font-sans overflow-x-hidden">
-            <Parallax ref={parallax} pages={8}>
-                <ParallaxLayer
-                    offset={0}
-                >
-                    <Banner
-                        handleClick={() => router.push('/amenities')}
-                    />
-                </ParallaxLayer>
-                <ParallaxLayer
-                    offset={1}
-                    speed={0.8}
-                >
-                    <Welcome/>
-                </ParallaxLayer>
-                <ParallaxLayer
-                    offset={2}
-                    speed={0.1}
-                >
-                    <Story/>
-                </ParallaxLayer>
-                <ParallaxLayer
-                    offset={3}
-                    speed={0.4}
-                >
-                    <Accommodations/>
-                </ParallaxLayer>
-                <ParallaxLayer
-                    speed={0.3}
-                    offset={4}
-                >
-                        <Dining/>
-                </ParallaxLayer>
-                <ParallaxLayer
-                    speed={0.3}
-                    offset={5}
-                >
-                    <Meetings/>
-                </ParallaxLayer>
-                <ParallaxLayer
-                    speed={0.4}
-                    offset={6}
-                >
-                    <Wellness/>
-                </ParallaxLayer>
-            </Parallax>
-
-
+        <main className="w-screen h-dvh font-sans overflow-x-hidden">
+            <Banner
+                handleClick={() => router.push('/amenities')}
+            />
+            <Welcome/>
+            <Story/>
+            <Accommodations/>
+            <Dining/>
+            <Meetings/>
+            <Wellness/>
         </main>
     )
 }
